ba33930a52fcd837e53eb65f5f26076381e2876f
Eric Njogu <kunadawa@gmail.com>
2019-01-18 05:17:57 +0000

    make the text input a controlled component

--- src/App.js
+++ src/App.js
@@ -3,17 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    usemi:'',
+  }
+
+updateUsemi = (usemi) => this.setState(
+  {
+    usemi:usemi,
+  }
+);
+  
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">ReactND - Coding Practice</h1>
+          <h1 className="App-title">Exercise 1 - Controlled Components</h1>
         </header>
         <div className="container">
-          <input type="text" placeholder="Say Something" />
+          <input type="text" placeholder="Say Something" onChange = {(event) => this.updateUsemi(event.target.value)}/>
           <p className="echo">Echo:</p>
-          <p>This should mirror the text you typed into the input field.</p>
+          <p>{this.state.usemi}</p>
         </div>
       </div>
     );
